96d1f6865a09304d4153a80fae22e572b83074ac
check321 <check321@aliyun.com>
2018-08-29 17:32:11 +0800

    init commit.

---
+++ src/main/java/net/check321/apigateway/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package net.check321.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.HttpStatus;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Component
+public class AuthFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        String token = request.getHeader("token");
+        if (!"test_token".equals(token)) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/net/check321/apigateway/filter/SeedFilter.java
@@ -0,0 +1,37 @@
+package net.check321.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Component
+public class SeedFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletResponse response = ctx.getResponse();
+
+        response.setHeader("Seed", UUID.randomUUID().toString());
+        return null;
+    }
+}
